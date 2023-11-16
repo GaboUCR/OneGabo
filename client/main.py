69cb7f1bc050cbf42448bfe72e11caf8b046d370
GaboUCR <gabriel.gamboavargas@ucr.ac.cr>
2023-11-16 04:08:50 -0600

    python client can send and receive messages

---
+++ client/main.py
@@ -0,0 +1,12 @@
+import asyncio
+import websockets
+
+async def websocket_client():
+    uri = "ws://127.0.0.1:8080"  # Reemplaza con la URI de tu servidor WebSocket
+    async with websockets.connect(uri) as websocket:
+        binary_message = bytes([0])
+        await websocket.send(binary_message)
+        response = await websocket.recv()
+        print(f"Received from server: {response}")
+
+asyncio.run(websocket_client())
