55b9f2220e707fde1e56310e4dab88ca2cce9091
GaboUCR <gabriel.gamboavargas@ucr.ac.cr>
2023-11-19 00:57:47 -0600

    El cliente envia archivos exitosamente

--- client/file_manager.py
+++ client/file_manager.py
@@ -1,6 +1,15 @@
 import os
 import datetime
 import asyncio
+import struct
+from enum import Enum
+
+class MsgCode (Enum):
+    InitialMessage = 0,
+    CreateFile = 1,
+    UpdateFile = 2,
+    NewFile = 3,
+    DeleteFile = 4
 
 def get_files_info(directory_path):
     files_info = []
@@ -12,11 +21,33 @@ def get_files_info(directory_path):
             files_info.append((filename, last_modified_date))
     return files_info
 
-async def periodic_file_check():
+def build_message(operation_code, filename, file_content):
+    # Empaquetar el código de operación (MsgCode.UpdateFile) en 4 bytes (little-endian)
+    operation_bytes = struct.pack('<I', operation_code)
+
+    # Empaquetar el tamaño del nombre del archivo en 4 bytes (little-endian)
+    filename_size_bytes = struct.pack('<I', len(filename))
+
+    # Convertir el nombre del archivo a bytes
+    filename_bytes = filename.encode()
+
+    # Construir el mensaje completo
+    message = operation_bytes + filename_size_bytes + filename_bytes + file_content
+    return message
+
+
+async def periodic_file_check(websocket, sent_files):
     while True:
-        print("Revisando archivos...")
-        files_info = get_files_info("disk")
-        for filename, modified_date in files_info:
-            print(f"Archivo: {filename}, Última modificación: {modified_date}")
+        current_files_info = get_files_info("disk")
+        for filename, modified_date in current_files_info:
+            # Comprobar si el archivo es nuevo o ha sido modificado desde el último envío
+            if filename not in sent_files or sent_files[filename] != modified_date:
+                with open(os.path.join("disk", filename), 'rb') as file:
+                    file_content = file.read()
+                    message = build_message(MsgCode.UpdateFile.value[0], filename, file_content)
+                    await websocket.send(message)
+                    
+                    # Actualizar el diccionario con la nueva fecha de modificación
+                    sent_files[filename] = modified_date
 
         await asyncio.sleep(3)
--- client/main.py
+++ client/main.py
@@ -1,15 +1,22 @@
 import asyncio
 import websockets
 from file_manager import periodic_file_check
+from enum import Enum
+
+class MsgCode (Enum):
+    InitialMessage = 0,
+    CreateFile = 1,
+    UpdateFile = 2,
+    NewFile = 3,
+    DeleteFile = 4
 
 async def websocket_client():
     uri = "ws://127.0.0.1:8080"  # Reemplaza con la URI de tu servidor WebSocket
-    async with websockets.connect(uri) as websocket:
-        binary_message = bytes([0])
-        await websocket.send(binary_message)
-        response = await websocket.recv()
-        print(f"Received from server: {response}")
+    sent_files = {}
 
-# asyncio.run(websocket_client())
-asyncio.run(periodic_file_check())
+    async with websockets.connect(uri) as websocket:
+        await periodic_file_check(websocket, sent_files)
+
+asyncio.run(websocket_client())
+# asyncio.run(periodic_file_check())
 
