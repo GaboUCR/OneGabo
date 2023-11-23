082208f14c51bd1c23e0b33053da24f1b283d2e6
GaboUCR <gabriel.gamboavargas@ucr.ac.cr>
2023-11-23 04:46:13 -0600

    Deleting files successfully

--- client/file_manager.py
+++ client/file_manager.py
@@ -3,39 +3,54 @@ import datetime
 import asyncio
 from message_handler import create_update_message, create_delete_message
 
-def get_files_info(directory_path):
+def get_files_and_folders_info(directory_path):
     files_info = []
+    folders_info = []
     for root, dirs, files in os.walk(directory_path):
+        # Include directory information
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            folders_info.append(os.path.relpath(dir_path, directory_path)) 
+
+        # Include file information
         for filename in files:
             full_path = os.path.join(root, filename)
-            if os.path.isfile(full_path):
-                modified_time = os.path.getmtime(full_path)
-                last_modified_date = datetime.datetime.fromtimestamp(modified_time)
-                relative_path = os.path.relpath(full_path, directory_path)
-                files_info.append((relative_path, last_modified_date))
-    return files_info
+            modified_time = os.path.getmtime(full_path)
+            last_modified_date = datetime.datetime.fromtimestamp(modified_time)
+            files_info.append((os.path.relpath(full_path, directory_path), last_modified_date))
+    
+    return (files_info, folders_info)
 
-async def periodic_file_check(websocket, sent_files):
+async def periodic_file_check(websocket, sent_files, sent_folders):
     while True:
-        current_files_info = get_files_info("disk")
-        current_file_names = set([file_info[0] for file_info in current_files_info])
+        current_files_info, current_folders_info = get_files_and_folders_info("disk") 
+        current_files_names = set([file_info[0] for file_info in current_files_info])
 
         # Check for new or modified files
         for filename, modified_date in current_files_info:
-            if filename not in sent_files or sent_files[filename] != modified_date:
+            if filename not in sent_files or sent_files[filename] != modified_date: # possible bug, only checks if dates are different
                 with open(os.path.join("disk", filename), 'rb') as file:
                     file_content = file.read()
                     message = create_update_message(filename, file_content)
                     await websocket.send(message)
-                    
                     sent_files[filename] = modified_date
-
+            
         # Check for deleted files
-        deleted_files = set(sent_files.keys()) - current_file_names
-        for filename in deleted_files:
+        deleted_items = set(sent_files.keys()) - current_files_names
+        for filename in deleted_items:
             message = create_delete_message(filename)
             await websocket.send(message)
-
-            del sent_files[filename]  # Remove from sent_files dictionary
+            del sent_files[filename] 
+        
+        # Check for deleted folders
+        deleted_folders =  sent_folders - set(current_folders_info) 
+        for folder_path in deleted_folders:
+            message = create_delete_message(folder_path)
+            await websocket.send(message)
+            sent_folders.remove(folder_path)
+        
+        # Adding new files if there's any
+        for folder_path in current_folders_info:
+            sent_folders.add(folder_path)
 
         await asyncio.sleep(0.314)
--- client/main.py
+++ client/main.py
@@ -5,8 +5,9 @@ from file_manager import periodic_file_check
 async def websocket_client():
     uri = "ws://127.0.0.1:8080"  # Reemplaza con la URI de tu servidor WebSocket
     sent_files = {}
+    sent_folders = set()
 
     async with websockets.connect(uri) as websocket:
-        await periodic_file_check(websocket, sent_files)
+        await periodic_file_check(websocket, sent_files, sent_folders)
 
 asyncio.run(websocket_client())
