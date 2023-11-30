cfe519e223a6c96b6de098c4f4a7c3a46b7c5794
GaboUCR <gabriel.gamboavargas@ucr.ac.cr>
2023-11-29 18:54:11 -0600

    Client Retry Mechanism for Connection Failures

--- client/websocket_client.py
+++ client/websocket_client.py
@@ -1,10 +1,15 @@
 from file_manager import periodic_file_check, load_state_from_disk
 import websockets
+import asyncio
 
 async def websocket_handler(uri, base_path="disk"):
-    # uri = "ws://127.0.0.1:8080"  # Reemplaza con la URI de tu servidor WebSocket
-
     sent_files, sent_folders = load_state_from_disk()
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                await periodic_file_check(websocket, sent_files, sent_folders, base_path)
 
-    async with websockets.connect(uri) as websocket:
-        await periodic_file_check(websocket, sent_files, sent_folders, base_path)
+                break
+        except (ConnectionRefusedError, websockets.exceptions.WebSocketException) as e:
+            print(f"Connection failed: {e}, retrying in 3 seconds...")
+            await asyncio.sleep(3)  # Wait for 3 seconds before retrying
