a17e966a89fc914793f44d25d0c97063bf3b2787
GaboUCR <gabriel.gamboavargas@ucr.ac.cr>
2023-12-19 03:53:03 -0600

    added db::user::get_user and hashed the password

--- server/src/db/user.rs
+++ server/src/db/user.rs
@@ -1,80 +1,84 @@
 //server/src/db/user.rs
 use tokio_rusqlite::{Connection as AsyncConnection, Error as TokioRusqliteError};
 use rusqlite::params;
-
+use bcrypt::{hash, verify, DEFAULT_COST};
 pub struct User {
     pub name: String,
     pub password: String
 }
 
 pub async fn add_user(user: User) -> Result<(), TokioRusqliteError> {
-    // Establecer conexión con la base de datos
-    let conn = match AsyncConnection::open("onegabo.db").await {
-        Ok(conn) => conn,
-        Err(e) => {
-            eprintln!("Error al conectar con la base de datos: {:?}", e);
-            return Err(e);
+
+    let conn = match AsyncConnection::open("./onegabo.db").await {
+        Ok(conn) => {
+            conn
         },
+        Err(e) => {
+            eprintln!("Error al abrir la base de datos: {:?}", e);
+            return Err(e);
+        }
     };
 
-    // Ejecutar operaciones de base de datos
+    let hashed_password = match hash(&user.password, DEFAULT_COST) {
+        Ok(h) => {
+            h
+        },
+        Err(e) => {
+            eprintln!("Error al hashear la contraseña: {:?}", e);
+            return Err(TokioRusqliteError::Other(Box::new(e)));
+        }
+    };
+
     let result = conn.call(move |conn| {
-        // Crear tabla si no existe
-        if let Err(e) = conn.execute(
+        conn.execute(
             "CREATE TABLE IF NOT EXISTS user (
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL UNIQUE,
                 password TEXT NOT NULL
             )",
             [],
-        ) {
-            eprintln!("Error al crear la tabla: {:?}", e);
-            return Err(tokio_rusqlite::Error::Rusqlite(e));
-        }
+        )?;
 
-        // Insertar usuario
-        match conn.execute(
+        conn.execute(
             "INSERT INTO user (name, password) VALUES (?1, ?2)",
-            params![user.name, user.password],
-        ) {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                eprintln!("Error al insertar el usuario: {:?}", e);
-                Err(tokio_rusqlite::Error::Rusqlite(e))
-            },
-        }
+            params![user.name, hashed_password],
+        )?;
+
+        Ok(())
     }).await;
 
-    // Manejar resultado de operaciones de base de datos
     match result {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            eprintln!("Error durante la operación de base de datos: {:?}", e);
-            Err(e)
+        Ok(_) => {
+            Ok(())
         },
+        Err(e) => {
+            eprintln!("Error durante las operaciones en la base de datos: {:?}", e);
+            Err(e)
+        }
     }
 }
 
-// pub async fn get_user(user:User) -> Result<(), TokioRusqliteError>{
-//     let conn = AsyncConnection::open("onegabo.db").await?;
+pub async fn get_user(user: User) -> Result<bool, TokioRusqliteError> {
+    let conn = AsyncConnection::open("onegabo.db").await?;
 
-//     let data = conn.call( move |conn| {
-//         conn.execute(
-//             "CREATE TABLE user (
-//                 id    INTEGER PRIMARY KEY,
-//                 name  TEXT NOT NULL UNIQUE,
-//                 password TEXT NOT NULL
-//             )",
-//             [],
-//         )?;
+    let result = conn.call(move |conn| {
+        let mut stmt = conn.prepare("SELECT password FROM user WHERE name = ?1")?;
+        let mut rows = stmt.query(params![&user.name])?;
 
-//         conn.execute(
-//             "INSERT INTO  (name, data) VALUES (?1, ?2)",
-//             params![user.name, user.password],
-//         )?;
+        if let Some(row) = rows.next()? {
+            let hashed_password: String = row.get(0)?;
+            match verify(&user.password, &hashed_password) {
+                Ok(valid) => Ok(valid),
+                Err(e) => {
+                    eprintln!("Error al verificar la contraseña: {:?}", e);
+                    let other = TokioRusqliteError::Other(Box::new(e));
+                    Err(other)
+                }
+            }
+        } else {
+            Ok(false)  // Usuario no encontrado
+        }
+    }).await?;
 
-//         Ok(())
-//     }).await?;
-
-//     Ok(())
-// }
+    Ok(result)
+}
